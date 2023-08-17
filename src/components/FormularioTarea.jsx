bea5b193175422e602a01fef85dc9ea8b6eee895
Goddzy <renatotrevisiolmontiel@gmail.com>
2023-08-16 21:11:16 -0300

    guardé las tareas en el localStorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,10 +1,21 @@
 import { Button, Form } from "react-bootstrap";
 import ListaTarea from "./ListaTarea";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FormularioTarea = () => {
+  //buscar las tareas del localStorage si es que existen
+  const tareasLocalStorage = JSON.parse(localStorage.getItem('arregloTareasKey')) || [];
+
   const [tarea, setTarea] = useState("");
-  const [arregloTareas, setArregloTareas] = useState([]);
+  const [arregloTareas, setArregloTareas] = useState(tareasLocalStorage);
+
+  //ciclo de vida de un componente 
+  //el arreglo vacío permite que el useEffect solamente se ejecute en el montaje, no en la actualización del proyecto (el montaje es cuando la página carga). Si quiero que se ejecuten en ambos simplemente se borra el array vacío. En este caso, al colocar arregloTareas en el array, estoy diciendo que el useEffect se ejecute solamente en el montaje y actualización del arregloTareas, es decir, cuando el state de éste sea modificado
+  useEffect(()=>{
+    localStorage.setItem('arregloTareasKey', JSON.stringify(arregloTareas));
+    console.log('prueba del ciclo de vida');
+  }, [arregloTareas])
+   
 
   function handleSubmit(e) {
     e.preventDefault();
